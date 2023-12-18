d7221ecc0cff2ad2daa0767254b686ceb9f2ab47
takhabarova <takhabarova@ozon.ru>
2023-12-18 14:48:48 +0100

    add parsing environment variables

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
+	"github.com/caarlos0/env/v6"
 	"github.com/go-resty/resty/v2"
 
 	"github.com/Gamilkarr/stattrack/internal/service"
 )
 
 var UpdateMetricsURL = "http://%s/update/{type}/{name}/{value}"
+
+type Config struct {
+	Address        string `env:"ADDRESS"`
+	PoolInterval   int64  `env:"POLL_INTERVAL"`
+	ReportInterval int64  `env:"REPORT_INTERVAL"`
+}
 
 type Agent struct {
 	Client  *resty.Client
@@ -40,7 +48,24 @@ func main() {
 		Metrics: new(service.Metrics),
 	}
 
+	var cfg Config
+
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	parseFlags()
+
+	if cfg.Address != "" {
+		flagRunAddr = cfg.Address
+	}
+	if cfg.PoolInterval != 0 {
+		pollInterval = time.Duration(cfg.PoolInterval) * time.Second
+	}
+	if cfg.ReportInterval != 0 {
+		reportInterval = time.Duration(cfg.ReportInterval) * time.Second
+	}
 
 	for {
 		time.Sleep(pollInterval)
